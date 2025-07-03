3bddb2864f5583705f04400a87c2ec253a636ff3
Angel D. Munoz <scythes.lol@gmail.com>
2025-07-03 12:48:51 -0600

    feat(env): enhance environment variable handling and add PlatformOps interface
    - Refactor environment variable prefix checks to use infix notation.
    - Introduce PlatformOps type with methods for OS checks and environment variable management.
    - Implement Create function to instantiate PlatformOps with provided arguments.
    - Update tests to utilize the new PlatformOps interface for better structure and clarity.

--- src/Perla/Env.fs
+++ src/Perla/Env.fs
@@ -1,4 +1,3 @@
-[<RequireQualifiedAccess>]
 module Perla.Env
 
 open System
@@ -84,7 +83,7 @@ let getGroups (regex: Regex) (input: string) =
 let (|PerlaPrefixed|_|) line =
 
   match getGroups envVarRegex line with
-  | [ _; key; value ] when key.StartsWith(PerlaEnvPrefix) ->
+  | [ _; key; value ] when key.StartsWith PerlaEnvPrefix ->
     ValueSome(key, value)
   | _ -> ValueNone
 
@@ -92,7 +91,7 @@ let (|PerlaPrefixed|_|) line =
 let (|NotPerlaPrefixed|_|) line =
 
   match getGroups envVarRegex line with
-  | [ _; key; value ] when not(key.StartsWith(PerlaEnvPrefix)) ->
+  | [ _; key; value ] when not(key.StartsWith PerlaEnvPrefix) ->
     ValueSome(key, value)
   | _ -> ValueNone
 
@@ -117,3 +116,73 @@ let LoadEnvFiles(files: string<SystemPath> seq) =
         varContent
       )
     | _ -> ()
+
+type PlatformOpsArgs = {
+  GetEnvVars: unit -> (string * string) list
+  ReadFile: string<SystemPath> -> string[]
+}
+
+type PlatformOps =
+  abstract member IsWindows: unit -> bool
+  abstract member PlatformString: unit -> string
+  abstract member ArchString: unit -> string
+  abstract member GetEnvContent: unit -> string option
+  abstract member LoadEnvFiles: files: string<SystemPath> seq -> unit
+
+let Create(args: PlatformOpsArgs) : PlatformOps =
+  { new PlatformOps with
+      member _.IsWindows() =
+        RuntimeInformation.IsOSPlatform OSPlatform.Windows
+
+      member _.PlatformString() =
+        if RuntimeInformation.IsOSPlatform OSPlatform.Windows then
+          "win32"
+        else if RuntimeInformation.IsOSPlatform OSPlatform.Linux then
+          "linux"
+        else if RuntimeInformation.IsOSPlatform OSPlatform.OSX then
+          "darwin"
+        else if RuntimeInformation.IsOSPlatform OSPlatform.FreeBSD then
+          "freebsd"
+        else
+          failwith "Unsupported OS"
+
+      member _.ArchString() =
+        match RuntimeInformation.OSArchitecture with
+        | Architecture.Arm -> "arm"
+        | Architecture.Arm64 -> "arm64"
+        | Architecture.X64 -> "x64"
+        | Architecture.X86 -> "ia32"
+        | _ -> failwith "Unsupported Architecture"
+
+      member _.GetEnvContent() = option {
+        let env = [
+          for (key, value) in args.GetEnvVars() do
+            if key.StartsWith PerlaEnvPrefix then
+              key.Replace(PerlaEnvPrefix, String.Empty), value
+        ]
+
+        let content =
+          (StringBuilder(), env)
+          ||> List.fold(fun sb (key, value) ->
+            sb.Append $"""export const %s{key} = "%s{value}";""")
+          |> _.ToString()
+
+        if String.IsNullOrWhiteSpace content then
+          return! None
+        else
+          return content
+      }
+
+      member _.LoadEnvFiles files =
+
+        for line in Seq.collect args.ReadFile files do
+          match line with
+          | PerlaPrefixed(varName, varContent) ->
+            Environment.SetEnvironmentVariable(varName, varContent)
+          | NotPerlaPrefixed(varName, varContent) ->
+            Environment.SetEnvironmentVariable(
+              $"{PerlaEnvPrefix}{varName}",
+              varContent
+            )
+          | _ -> ()
+  }
--- src/Perla/Env.fsi
+++ src/Perla/Env.fsi
@@ -1,4 +1,3 @@
-[<RequireQualifiedAccess>]
 module Perla.Env
 
 open FSharp.UMX
@@ -10,3 +9,17 @@ val ArchString: string
 val internal getPerlaEnvVars: unit -> (string * string) list
 val GetEnvContent: unit -> string option
 val LoadEnvFiles: files: string<SystemPath> seq -> unit
+
+type PlatformOpsArgs = {
+  GetEnvVars: unit -> (string * string) list
+  ReadFile: string<SystemPath> -> string[]
+}
+
+type PlatformOps =
+  abstract member IsWindows: unit -> bool
+  abstract member PlatformString: unit -> string
+  abstract member ArchString: unit -> string
+  abstract member GetEnvContent: unit -> string option
+  abstract member LoadEnvFiles: files: string<SystemPath> seq -> unit
+
+val Create: args: PlatformOpsArgs -> PlatformOps
