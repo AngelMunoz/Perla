d4e208e542be701322117b37fbf2bd350da350b3
Angel D. Munoz <scythes.lol@gmail.com>
2025-07-06 11:28:47 -0600

    refactor(plugin): enhance PluginLoadError handling and update ExtensibilityService
    - Improved error messages in PluginLoadError module.
    - Refactored ExtensibilityService interface to include new methods for plugin management.
    - Removed obsolete types and methods related to plugin caching.

--- src/Perla.Plugins/Registry.fs
+++ src/Perla.Plugins/Registry.fs
@@ -19,6 +19,16 @@ type PluginLoadError =
   | EvaluationFailed of evalFailure: exn
   | NoPluginFound of pluginName: string
 
+
+module PluginLoadError =
+  let AsString =
+    function
+    | SessionExists -> "A session with this ID already exists."
+    | BoundValueMissing -> "The expected bound value is missing."
+    | AlreadyLoaded name -> $"Plugin '{name}' is already loaded."
+    | EvaluationFailed ex -> $"Evaluation failed: {ex.Message}"
+    | NoPluginFound name -> $"No plugin found with the name '{name}'."
+
 module SessionFactory =
   let inline create stdout stderr =
     FsiEvaluationSession.Create(
@@ -36,119 +46,6 @@ module ScriptReflection =
     | Some bound, _ -> Some bound.Value
     | None, Some bound -> Some bound.Value
     | None, None -> None
-
-type SessionCache<'Cache
-  when 'Cache: (static member SessionCache:
-    Lazy<Dictionary<string, FsiEvaluationSession>>)> = 'Cache
-
-type PluginCache<'Cache
-  when 'Cache: (static member PluginCache: Lazy<Dictionary<string, PluginInfo>>)>
-  = 'Cache
-
-type StdoutStderr<'Writer
-  when 'Writer: (static member Stdout: TextWriter)
-  and 'Writer: (static member Stderr: TextWriter)> = 'Writer
-
-
-[<RequireQualifiedAccess>]
-module PluginRegistry =
-
-  let inline GetRunnablePlugins<'Cache when PluginCache<'Cache>> order = [
-    for name in order do
-      match 'Cache.PluginCache.Value.TryGetValue(name) with
-      | true, plugin ->
-        match plugin.shouldProcessFile, plugin.transform with
-        | ValueSome st, ValueSome t -> {
-            plugin = plugin
-            shouldTransform = st
-            transform = t
-          }
-        | _ -> ()
-      | false, _ -> ()
-  ]
-
-  let inline GetPluginList<'Cache when PluginCache<'Cache>>() =
-    'Cache.PluginCache.Value.Values |> Seq.toList
-
-  let inline RunPlugins<'Cache when PluginCache<'Cache>>
-    (pluginOrder: string list)
-    fileInput
-    =
-    async {
-      let plugins = GetRunnablePlugins<'Cache> pluginOrder
-
-      let inline folder result next =
-        cancellableValueTask {
-          match next.shouldTransform result.extension with
-          | true -> return! next.transform result
-          | false -> return result
-        }
-        |> Async.AwaitCancellableValueTask
-
-      return! plugins |> AsyncSeq.ofSeq |> AsyncSeq.foldAsync folder fileInput
-    }
-
-  let inline HasPluginsForExtension<'Cache when PluginCache<'Cache>> extension =
-    let list = GetPluginList<'Cache>()
-
-    list
-    |> List.exists(fun plugin ->
-
-      match plugin.shouldProcessFile with
-      | ValueSome f -> f extension
-      | _ -> false)
-
-  let inline LoadFromCode<'Cache when PluginCache<'Cache>>(plugin: PluginInfo) =
-    if 'Cache.PluginCache.Value.TryAdd(plugin.name, plugin) then
-      Ok()
-    else
-      Error(AlreadyLoaded plugin.name)
-
-[<Class; Sealed>]
-type PluginRegistry =
-
-  static member inline LoadFromText<'Cache, 'Writer
-    when PluginCache<'Cache> and SessionCache<'Cache> and StdoutStderr<'Writer>>
-    (id, content, cancellationToken)
-    =
-    result {
-      let mutable discard: FsiEvaluationSession = Unchecked.defaultof<_>
-      let sessionCache = 'Cache.SessionCache
-
-      do!
-        sessionCache.Value.TryGetValue(id, &discard)
-        |> Result.requireFalse SessionExists
-
-      let plugins = 'Cache.PluginCache
-
-      let session = SessionFactory.create 'Writer.Stdout 'Writer.Stderr
-
-      let evaluation, _ =
-        session.EvalInteractionNonThrowing(
-          content,
-          ?cancellationToken = cancellationToken
-        )
-
-      let! foundValue = result {
-        let! result =
-          evaluation |> Result.ofChoice |> Result.mapError EvaluationFailed
-
-        match result with
-        | Some result -> return result
-        | None ->
-          return!
-            ScriptReflection.findPlugin session
-            |> Result.requireSome BoundValueMissing
-      }
-
-      if foundValue.ReflectionType = typeof<PluginInfo> then
-        sessionCache.Value.Add(id, session)
-        let plugin: PluginInfo = unbox foundValue.ReflectionValue
-        plugins.Value.Add(id, plugin)
-        return plugin
-      else
-        return! Error(NoPluginFound id)
-    }
 
 [<Interface>]
 type PluginManager =
--- src/Perla.Plugins/Registry.fsi
+++ src/Perla.Plugins/Registry.fsi
@@ -1,6 +1,5 @@
 namespace Perla.Plugins.Registry
 
-open System.Collections.Generic
 open System.IO
 open System.Threading
 open FSharp.Compiler.Interactive.Shell
@@ -15,18 +14,8 @@ type PluginLoadError =
   | EvaluationFailed of evalFailure: exn
   | NoPluginFound of pluginName: string
 
-
-type SessionCache<'Cache
-  when 'Cache: (static member SessionCache:
-    Lazy<Dictionary<string, FsiEvaluationSession>>)> = 'Cache
-
-type PluginCache<'Cache
-  when 'Cache: (static member PluginCache: Lazy<Dictionary<string, PluginInfo>>)>
-  = 'Cache
-
-type StdoutStderr<'Writer
-  when 'Writer: (static member Stdout: TextWriter)
-  and 'Writer: (static member Stderr: TextWriter)> = 'Writer
+module PluginLoadError =
+  val AsString: PluginLoadError -> string
 
 
 // Interface-based services for plugin registry (new implementation)
@@ -64,28 +53,3 @@ type PluginManager =
 [<RequireQualifiedAccess>]
 module PluginManager =
   val Create: stdout: TextWriter * stderr: TextWriter -> PluginManager
-
-[<RequireQualifiedAccess>]
-module PluginRegistry =
-  val inline GetRunnablePlugins<'Cache when PluginCache<'Cache>> :
-    string seq -> RunnablePlugin list
-
-  val inline GetPluginList<'Cache when PluginCache<'Cache>> :
-    unit -> PluginInfo list
-
-  val inline RunPlugins<'Cache when PluginCache<'Cache>> :
-    pluginOrder: string list -> fileInput: FileTransform -> Async<FileTransform>
-
-  val inline HasPluginsForExtension<'Cache when PluginCache<'Cache>> :
-    extension: string -> bool
-
-  val inline LoadFromCode<'Cache when PluginCache<'Cache>> :
-    PluginInfo -> Result<unit, PluginLoadError>
-
-[<Class; Sealed>]
-type PluginRegistry =
-
-  static member inline LoadFromText<'Cache, 'Writer
-    when PluginCache<'Cache> and SessionCache<'Cache> and StdoutStderr<'Writer>> :
-    id: string * content: string * ?cancellationToken: CancellationToken ->
-      Result<PluginInfo, PluginLoadError>
