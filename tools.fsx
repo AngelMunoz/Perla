80ff1fbe426720387f487f4d49da51a684c58856
Angel D. Munoz <scythes.lol@gmail.com>
2025-07-16 23:01:37 -0600

    chore: bump version

--- tools.fsx
+++ tools.fsx
@@ -21,7 +21,7 @@ let libraries = [ "Perla.PkgManager"; "Perla.Plugins"; "Perla.Logger" ]
 let NugetApiKey = EnvVar.getOrFail "NUGET_DEPLOY_KEY"
 
 [<Literal>]
-let PackageVersion = "1.0.0-beta-026"
+let PackageVersion = "1.0.0-beta-027"
 
 let fsSources =
   Glob.create "*.fsx"
