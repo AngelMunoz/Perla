e336fde100f3f1f03e41392fc15a6af05db1ccc7
Angel D. Munoz <scythes.lol@gmail.com>
2025-07-24 11:53:28 -0700

    fix(utils): update URL construction logic - Refactor URL prefix handling based on hostname

--- docs/src/utils.ts
+++ docs/src/utils.ts
@@ -6,8 +6,12 @@ export function buildUrl(
 ) {
   const name = filename.endsWith(".html") ? filename : `${filename}.html`;
   const isBlogOrContent = kind === "Blogs" || kind === "Content";
+  const prefix =
+    window.location.hostname === "angelmunoz.github.io"
+      ? "/Perla/assets"
+      : "/assets";
 
-  let url = `#/assets/${kind.toLowerCase()}`;
+  let url = `${prefix}/${kind.toLowerCase()}`;
 
   if (!isBlogOrContent && version) {
     url += `/${version ?? "v0"}`;
